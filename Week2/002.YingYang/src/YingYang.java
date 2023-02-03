36966b11384b337e3b70ac0c14f7ce4e23c55ecc
Nikulair <niekhoevenaars@gmail.com>
2023-02-03 22:22:39 +0100

    YingYang done

--- Week2/002.YingYang/src/YingYang.java
+++ Week2/002.YingYang/src/YingYang.java
@@ -27,14 +27,54 @@ public class YingYang extends Application {
         draw(new FXGraphics2D(canvas.getGraphicsContext2D()));
     }
 
-
     public void draw(FXGraphics2D graphics)
     {
-        graphics.setTransform(new AffineTransform());
-        graphics.setBackground(Color.white);
-        graphics.clearRect(0, 0, (int) canvas.getWidth(), (int) canvas.getHeight());
-    }
 
+        graphics.setTransform(new AffineTransform());
+        graphics.setBackground(Color.lightGray);
+        graphics.clearRect(0, 0, (int) canvas.getWidth(), (int) canvas.getHeight());
+
+        Area yingYangSide = new Area();
+
+
+        double scale;
+        if (canvas.getHeight()>canvas.getWidth()){
+            graphics.translate(0,(canvas.getHeight()-canvas.getWidth())/2);
+            scale = canvas.getWidth()/2;
+        } else {
+            graphics.translate((canvas.getWidth()-canvas.getHeight())/2,0);
+            scale = canvas.getHeight()/2;
+        }
+        graphics.scale(2,2);
+
+        // circle
+        yingYangSide.add(new Area(new Ellipse2D.Double(0,0,scale, scale)));
+
+        // remove right side
+        yingYangSide.subtract(new Area(new Rectangle2D.Double(.5*scale,0,.5*scale,scale)));
+
+        // add upper halfcircle
+        yingYangSide.add(new Area(new Ellipse2D.Double(.25*scale,0,.5*scale,.5*scale)));
+
+        // remove lower halfcircle
+        yingYangSide.subtract(new Area(new Ellipse2D.Double(.25*scale,.5*scale,.5*scale,.5*scale)));
+
+        // add smol circle
+        yingYangSide.add(new Area(new Ellipse2D.Double(.4375*scale,.6875*scale,.125*scale,.125*scale)));
+
+        // remove smol circle
+        yingYangSide.subtract(new Area(new Ellipse2D.Double(.4375*scale,.1875*scale,.125*scale,.125*scale)));
+
+
+        graphics.draw(yingYangSide);
+        graphics.setColor(Color.white);
+        graphics.fill(yingYangSide);
+        graphics.rotate(Math.toRadians(180), scale/2,scale/2);
+        graphics.setColor(Color.black);
+        graphics.draw(yingYangSide);
+        graphics.fill(yingYangSide);
+
+    }
 
     public static void main(String[] args)
     {
